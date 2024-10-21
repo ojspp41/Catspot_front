005a785b1f3a17e0617486e6725565d8c39d3399
ohjunseok <ojspp000@naver.com>
2024-10-21 15:08:26 +0900

    hell

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        Welcome to React Vite App!
+      </div>
+    </div>
+  )
+}
+
+export default App
