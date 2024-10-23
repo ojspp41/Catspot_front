45ef4e4d7b8c90897382f4407941ace3f7ae5a36
ojspp000@naver.com <ojspp000@naver.com>
2024-10-23 20:33:53 +0900

    navbar 만듬 사용법 props로 타이틀 주기 <Navbar title="빈강의실" /> 예시

--- src/components/ex.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ex = () => {
-  return (
-    <div>ex</div>
-  )
-}
--- src/pages/expage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const expage = () => {
-  return (
-    <div>expage</div>
-  )
-}
