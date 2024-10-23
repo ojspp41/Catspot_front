3b7f4a9ccc7364862900129d85b7d08641e04236
ojspp000@naver.com <ojspp000@naver.com>
2024-10-23 20:50:43 +0900

    app수정

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Navbar } from '../components/Navbar';
-
+import "../css/pages/room.css";
 const Room = () => {
   return (
-    <Navbar title="빈강의실" />
+    
+    <>
+        <Navbar title="빈강의실" />
+        <div class="room-centered-box">
+            <p class="room-centered-box-text">e box</p>
+        </div>
+
+
+
+    </>
   );
 };
 
