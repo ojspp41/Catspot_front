26f01ff4e6d087f6a2fe1f85e74ec60d358b089d
ojspp000@naver.com <ojspp000@naver.com>
2024-10-24 21:48:54 +0900

    sw

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,18 @@ import LibrarySite4 from "./pages/library_site4.jsx";
 import LibrarySite5 from "./pages/library_site5.jsx";
 import './App.css';
 
+import Room from './pages/room';
+import N1 from './pages/n1';
+import N2 from './pages/n2';
+import N3 from './pages/n3';
+import N4 from './pages/n4';
+
 function App() {
   return (
     <RecoilRoot>
       <Router>
         <Routes>
+          {/* Mainpage and Library routes */}
           <Route path="/" element={<Mainpage />} />
           <Route path="/Library" element={<Library />} />
           <Route path="/Library_site1" element={<LibrarySite1 />} />
@@ -21,9 +28,17 @@ function App() {
           <Route path="/Library_site3" element={<LibrarySite3 />} />
           <Route path="/Library_site4" element={<LibrarySite4 />} />
           <Route path="/Library_site5" element={<LibrarySite5 />} />
+          
+          {/* Room routes */}
+          <Route path="/room" element={<Room />} />
+          <Route path="/room/n1" element={<N1 />} />
+          <Route path="/room/n2" element={<N2 />} />
+          <Route path="/room/n3" element={<N3 />} />
+          <Route path="/room/n4" element={<N4 />} />
         </Routes>
       </Router>
     </RecoilRoot>
   );
 }
+
 export default App;
