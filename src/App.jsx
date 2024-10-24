89a72f73757c36889e1825f5dc6a711d270d2395
ojspp000@naver.com <ojspp000@naver.com>
2024-10-24 22:01:06 +0900

    sds

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,26 @@ function App() {
   return (
     <RecoilRoot>
       <Router>
-        <Routes>
-          {/* Mainpage and Library routes */}
-          <Route path="/" element={<Mainpage />} />
-          <Route path="/Library" element={<Library />} />
-          <Route path="/Library_site1" element={<LibrarySite1 />} />
-          <Route path="/Library_site2" element={<LibrarySite2 />} />
-          <Route path="/Library_site3" element={<LibrarySite3 />} />
-          <Route path="/Library_site4" element={<LibrarySite4 />} />
-          <Route path="/Library_site5" element={<LibrarySite5 />} />
-          
-          {/* Room routes */}
-          <Route path="/room" element={<Room />} />
-          <Route path="/room/n1" element={<N1 />} />
-          <Route path="/room/n2" element={<N2 />} />
-          <Route path="/room/n3" element={<N3 />} />
-          <Route path="/room/n4" element={<N4 />} />
-        </Routes>
+        <div className="App">
+
+          <Routes>
+            {/* Mainpage and Library routes */}
+            <Route path="/" element={<Mainpage />} />
+            <Route path="/Library" element={<Library />} />
+            <Route path="/Library_site1" element={<LibrarySite1 />} />
+            <Route path="/Library_site2" element={<LibrarySite2 />} />
+            <Route path="/Library_site3" element={<LibrarySite3 />} />
+            <Route path="/Library_site4" element={<LibrarySite4 />} />
+            <Route path="/Library_site5" element={<LibrarySite5 />} />
+            
+            {/* Room routes */}
+            <Route path="/room" element={<Room />} />
+            <Route path="/room/n1" element={<N1 />} />
+            <Route path="/room/n2" element={<N2 />} />
+            <Route path="/room/n3" element={<N3 />} />
+            <Route path="/room/n4" element={<N4 />} />
+          </Routes>
+        </div>
       </Router>
     </RecoilRoot>
   );
