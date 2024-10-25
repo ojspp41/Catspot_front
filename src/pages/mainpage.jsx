b915e62dfff0852a884a740665217f4157311c67
somin2352 <somin030502@gmail.com>
2024-10-25 21:59:09 +0900

    높이 수정

--- src/pages/mainpage.jsx
+++ src/pages/mainpage.jsx
@@ -5,8 +5,8 @@ import "./../css/pages/mainpage.css";
 export default function MainPage() {
   return (
     <div className="mainpage">
-      <p>빈 공간을 찾으신다면?</p>
-      <h1>CAT-SPOT</h1>
+      <p className="sub-title">빈 공간을 찾으신다면?</p>
+      <p className="title">CAT-SPOT</p>
       <Character />
       <MainButton />
     </div>
