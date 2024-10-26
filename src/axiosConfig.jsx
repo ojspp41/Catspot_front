5ba91e41fcd493196696f81a0b91456354f6fba4
ohjunseok <ojspp000@naver.com>
2024-10-27 01:38:32 +0900

    sdsdsds

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api', // 프록시를 위한 API 기본 URL
+  baseURL: 'https://catspot-dev.vercel.app', // 프록시를 위한 API 기본 URL
   headers: {
     'Content-Type': 'application/json',
   },
