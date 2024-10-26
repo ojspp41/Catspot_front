82e86026ba33009eff85f7b8dcfbeb350c71405c
ohjunseok <ojspp000@naver.com>
2024-10-27 02:15:30 +0900

    dsdsd

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '', // 프록시를 위한 API 기본 URL
+  baseURL: 'http://3.35.114.206', // 프록시를 위한 API 기본 URL
   headers: {
     'Content-Type': 'application/json',
   },
