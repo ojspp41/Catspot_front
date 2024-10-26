9d93a34c274fcb33c3a9fb65696eae8e23aa7bec
ohjunseok <ojspp000@naver.com>
2024-10-27 01:15:20 +0900

    vercel

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.35.114.206', // 백엔드 API 기본 URL
+  baseURL: '/api', // 프록시를 위한 API 기본 URL
   headers: {
     'Content-Type': 'application/json',
   },
